9e7e746e75a0b80a8cc6ddc4e8139c1e51a29a1b
gresm <78505251+gresm@users.noreply.github.com>
2022-02-07 12:26:15 +0100

    ~ .gitignore ~ setup_editor.py

--- setup_editor.py
+++ setup_editor.py
@@ -5,14 +5,24 @@ reqs = pt.Path("requirements-editor.txt")
 long_desc = pt.Path("README-engine.md")
 setup(
     name="cpaste-editor",
-    version="0.0.2",
+    version="0.0.3",
     author="gresm",
     description="Graphical interface of cpaste - a game engine written in python using pygame",
     long_description=long_desc.read_text(),
     long_description_content_type="text/markdown",
     install_requires=reqs.read_text().split("\n"),
-    packages=["cpaste_editor"],
-    package_dir={"cpaste_editor": "editor"},
+    packages=[
+        "cpaste_editor",
+        "cpaste_editor.fonts",
+        "cpaste_editor.gui_definitions",
+        "cpaste_editor.themes"
+    ],
+    package_dir={
+        "cpaste_editor": "editor",
+        "cpaste_editor.fonts": "editor/fonts",
+        "cpaste_editor.gui_definitions": "editor/gui_definitions",
+        "cpaste_editor.themes": "editor/themes"
+    },
     url="https://github.com/gresm/cpaste_game_engine",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
