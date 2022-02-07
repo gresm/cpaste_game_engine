1d9922766da81b89159909445834b30486158c1c
gresm <78505251+gresm@users.noreply.github.com>
2022-02-07 20:57:07 +0100

    ~ build.sh ~ editor/   ~ fonts/     ~ __init__.py     ~ current_theme.json <- ../current_theme.json   - themes.py ~ main/ ~ __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -1,4 +1,20 @@
 from . import run
+import argparse
+from os import system
 
 
-run()
+parser = argparse.ArgumentParser(description="Run cpaste game engine, or do some actions with it")
+parser.add_argument("-d", "--delete", action="store_true", help="Delete cpaste engine modules", default=None)
+args = parser.parse_args()
+
+if args.delete:
+    print("deleting cpaste-api")
+    system("pip uninstall -y cpaste-api")
+    print("deleting cpaste-core")
+    system("pip uninstall -y cpaste-core")
+    print("deleting cpaste-editor")
+    system("pip uninstall -y cpaste-editor")
+    print("deleting cpaste")
+    system("pip uninstall -y cpaste")
+else:
+    run()
