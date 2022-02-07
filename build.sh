9a4c0502b8dd23fa95b627fb955437d8f364aa91
gresm <78505251+gresm@users.noreply.github.com>
2022-02-07 19:48:20 +0100

    ~ build.sh ~ editor/   ~ fonts/ + __init__.py   ~ themes/ + __init__.py  ~ setup_editor.py

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 python3 setup.py all build
 python3 setup.py all sdist bdist_wheel
+twine upload -r testpypi dist/cpaste[-_]api*
+twine upload -r testpypi dist/cpaste[-_]core*
+twine upload -r testpypi dist/cpaste[-_]editor*
 twine upload -r testpypi dist/*
