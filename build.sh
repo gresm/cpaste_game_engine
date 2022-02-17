76cc6c1251e249f718fbe5c8458dc100ec2a3015
gresm <78505251+gresm@users.noreply.github.com>
2022-02-17 20:17:19 +0100

    ~ build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 python3 setup.py all build
 python3 setup.py all sdist bdist_wheel
-twine upload -r testpypi dist/cpaste[-_]api*
-twine upload -r testpypi dist/cpaste[-_]core*
-twine upload -r testpypi dist/cpaste[-_]editor*
-twine upload -r testpypi dist/cpaste[-_][0-9]*
-twine upload -r testpypi dist/*
+twine upload --skip-existing -r testpypi dist/*
