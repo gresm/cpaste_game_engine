2420480cca651c75413bf35005e6df06e5d4a35f
gresm <78505251+gresm@users.noreply.github.com>
2022-02-18 13:31:06 +0100

    ~ editor/ ~ tools/ ~ object_promise.py

--- editor/tools/object_promise.py
+++ editor/tools/object_promise.py
@@ -1,6 +1,18 @@
 from __future__ import annotations
 
 
+_required_magic_methods = set(dir(object))
+
+
+def _is_important_key(key: str):
+    return key in _required_magic_methods or (key.startswith("_") and not key.startswith("__")) or key == "solve"
+
+
+def _is_sub_important(key: str):
+    return key in {"__init__", "__getattribute__", "__setattr__", "__delattr__", "__get__"} or key == "solve"\
+           or (key.startswith("_") and not key.startswith("__"))
+
+
 class ObjectPromise:
     def __init__(self):
         self._solved = False
@@ -8,35 +20,52 @@ class ObjectPromise:
         self._actions: list[AttributePromise] = []
 
     def __getattribute__(self, item: str):
-        return ObjectPromise.__get(self, item)
+        return ObjectPromise._get(self, item, True)
 
     def __setattr__(self, key, value):
-        if key.startswith("_") or key == "solve":
-            return super(ObjectPromise, self).__setattr__(key, value)
-        if self._solved:
-            return setattr(self._solution, key, value)
-        return self._get_attr_promise(key, 1, value)
+        return ObjectPromise._set(self, key, value, True)
 
     def __delattr__(self, item):
-        if item.startswith("_") or item == "solve":
+        return ObjectPromise._del(self, item, True)
+
+    def __call__(self, *args, **kwargs):
+        return self._get("__call__", False)(*args, **kwargs)
+
+    def __float__(self, *args, **kwargs):
+        return self._get("__float__", False)(*args, **kwargs)
+
+    def __int__(self, *args, **kwargs):
+        return self._get("__int__", False)(*args, **kwargs)
+
+    def _get(self, item: str, imp):
+        run = _is_sub_important
+        if imp:
+            run = _is_important_key
+        if run(item):
+            return super(ObjectPromise, self).__getattribute__(item)
+        if self._solved:
+            return getattr(self._solution, item)
+        return self._get_attr_promise(item, 0)
+
+    def _set(self, item: str, value, imp):
+        run = _is_sub_important
+        if imp:
+            run = _is_important_key
+        if run(item):
+            return super(ObjectPromise, self).__setattr__(item, value)
+        if self._solved:
+            return setattr(self._solution, item, value)
+        return self._get_attr_promise(item, 1, value)
+
+    def _del(self, item: str, imp):
+        run = _is_sub_important
+        if imp:
+            run = _is_important_key
+        if run(item):
             return super(ObjectPromise, self).__delattr__(item)
         if self._solved:
             return delattr(self._solution, item)
         return self._get_attr_promise(item, 2)
-
-    def __call__(self, *args, **kwargs):
-        return ObjectPromise.__get(self, "__call__", True)(*args, **kwargs)
-
-    def __get(self, item: str, force=False):
-        if force:
-            if self._solved:
-                return getattr(self._solution, item, None)
-            return self._get_attr_promise(item, 0)
-        if item.startswith("_") or item == "solve":
-            return super(ObjectPromise, self).__getattribute__(item)
-        if self._solved:
-            return getattr(self._solution, item, None)
-        return self._get_attr_promise(item, 0)
 
     def _get_attr_promise(self, name: str, mode: int, extra=None):
         ret = AttributePromise(self, name, mode, extra)
@@ -59,6 +88,15 @@ class AttributePromise(ObjectPromise):
         self._name = name
         self._mode = mode
         self._extra = extra
+
+    def __getattribute__(self, item: str):
+        return ObjectPromise._get(self, item, False)
+
+    def __setattr__(self, key, value):
+        return ObjectPromise._set(self, key, value, False)
+
+    def __delattr__(self, item):
+        return ObjectPromise._del(self, item, False)
 
     def __get__(self, instance, owner):
         if instance is None or not self.solved:
